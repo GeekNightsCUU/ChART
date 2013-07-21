a3ed24759bb82b9294d860c657494b28aa022f6b
Ricardo Echavarria <rgechavarria@gmail.com>
2013-07-21 16:45:10 -0600

    Ninject Support

--- ChARTServices/Global.asax.cs
+++ ChARTServices/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
+using ChARTServices.Infrastructure;
 
 namespace ChARTServices
 {
@@ -19,6 +20,8 @@ namespace ChARTServices
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory());
         }
     }
 }
---
+++ ChARTServices/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Ninject;
+
+namespace ChARTServices.Infrastructure
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+
+        public NinjectControllerFactory()
+        {
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null
+                ? null
+                : (IController)ninjectKernel.Get(controllerType);        
+        }
+
+        private void AddBindings()
+        {
+            //Bindings go here
+        }
+    }
+}
