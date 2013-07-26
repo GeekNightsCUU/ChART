8d3fa7836cb7621b1764670d03c90f96ff04bd7f
Ricardo Echavarria <rgechavarria@gmail.com>
2013-07-26 10:44:28 -0600

    Geospatial Index

--- ChArt.Domain/Entities/Station.cs
+++ ChArt.Domain/Entities/Station.cs
@@ -8,8 +8,14 @@ namespace ChART.Domain.Entities
     {
         public Object Id { get; set; }
         public string Name { get; set; }
-        public string Latitude { get; set; }
-        public string Longitude { get; set; }
+        public double[] Location { get; set; }
+        public double Latitude { get {return Location[1];} }
+        public double Longitude { get{return Location[0];} }
         public string IconUrl { get; set; }
+
+        public Station()
+        {
+            Location = new double[2];
+        }
     }
 }
