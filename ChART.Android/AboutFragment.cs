127ec883a388352d6d1032b9d81b04e083abc204
Magio <aalbertobus@gmail.com>
2013-09-23 17:35:09 -0600

    Social Buttons URLS

--- ChART.Android/AboutFragment.cs
+++ ChART.Android/AboutFragment.cs
@@ -1,4 +1,4 @@
-using System;
+//using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +8,7 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Android.Net;
 using Xamarin.ActionbarSherlockBinding.App;
 
 namespace ChART.Android
@@ -17,13 +18,30 @@ namespace ChART.Android
 		public override void OnCreate (Bundle savedInstanceState)
 		{
 			base.OnCreate (savedInstanceState);
-			// Create your fragment here
 
 		}
 
 		public override View OnCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 		{
 			View view = inflater.Inflate (Resource.Layout.AboutNavigation, container, false);
+			var facebook = view.FindViewById<ImageView> (Resource.Id.imageView_facebook);
+			facebook.Click += (object sender, System.EventArgs e) => {
+				var uri = Uri.Parse ("http://www.facebook.com/geeknightscuu");
+				var intent = new Intent (Intent.ActionView, uri);
+				StartActivity (intent);     
+			};
+			var twitter = view.FindViewById<ImageView> (Resource.Id.imageView_twitter);
+			twitter.Click += (object sender, System.EventArgs e) => {
+				var uri = Uri.Parse ("http://www.twitter.com/geeknightscuu");
+				var intent = new Intent (Intent.ActionView, uri);
+				StartActivity (intent);     
+			};
+			var plus = view.FindViewById<ImageView> (Resource.Id.imageView_plus);
+			plus.Click += (object sender, System.EventArgs e) => {
+				var uri = Uri.Parse ("https://plus.google.com/communities/113864651382277557583");
+				var intent = new Intent (Intent.ActionView, uri);
+				StartActivity (intent);     
+			};
 			return view;
 		}
 
